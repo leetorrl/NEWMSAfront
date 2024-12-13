7374c962723d04fcd0a4e85497f32824392a77a5
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-12-13 14:29:25 +0900

    1213 동현) 점심이후 푸쉬

--- teamprojectvue/src/views/student/StudentQnAborad.vue
+++ teamprojectvue/src/views/student/StudentQnAborad.vue
@@ -1,17 +1,22 @@
 <template>
-      <div class="ml-4 font-sans flex justify-center">
-    <main class="flex justify-center w-[74.5rem]">
-      <section class="flex-1 p-6 m-2 bg-white border border-gray-500">
-        <h1 class="mb-5 text-2xl font-semibold">a문의사항</h1>
+     <div class="p-10 mt-10 ml-3">
+    <div>
+      <h1 class="text-3xl font-bold text-blue-900">문의사항
+      </h1>
+    </div>
+    <hr class="border-b border-[#eee] mt-4" />
+    <hr class="mt-3 border-b-4 border-blue-900" />
 
+    <main class="flex justify-center w-[68rem]">
+      <section class="flex-1 p-6 m-2 bg-white">
         <div >
-          <select class="border border-gray-500 mr-3" v-model="selectedlecture" name="" id="">
+          <select class="mr-3 border border-gray-500" v-model="selectedlecture" name="" id="">
             <option value="전체">전체</option>
-            <option v-for="lecture in lecturelist" :key="lecture.idx" :value="lecture.idx">
+            <option v-for="item in arr" :key="item.idx" :value="item.idx">
               {{  }}
             </option>
           </select>
-          <button  class="px-2 py-1 mr-1 text-white bg-blue-600 rounded hover:opacity-80">
+          <button @click="resetSort(1)" class="px-2 py-1 mr-1 text-white bg-blue-600 rounded hover:opacity-80">
             초기화
           </button>
         </div>
@@ -19,41 +24,42 @@
           <table class="w-full mb-5 border border-collapse border-gray-300">
             <thead>
               <tr class="bg-gray-100">
-                <th class="p-1 border border-gray-300 w-2/12">구분</th>
-                <th class="p-1 border border-gray-300 w-5/12">제목</th>
+                <th class="p-1 border border-gray-300 w-1/12">구분</th>
+                <th class="p-1 border border-gray-300 w-4/12">제목</th>
+                <th class="p-1 border border-gray-300 w-1/12">글쓴이</th>
+                <th class="p-1 border border-gray-300 w-2/12">작성일</th>
                 <th class="p-1 border border-gray-300 w-1/12">진행상태</th>
-                <th class="p-1 border border-gray-300 w-1/6">작성일</th>
-                <th class="p-1 border border-gray-300 w-1/12">조회수</th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="item in arr" :key="item.idx" class="text-center">
+               
                 <td 
-                  class="p-1 border border-gray-300 hover:underline hover:cursor-pointer hover:bg-gray-200">{{
-                    item.idx }}</td>
-                <td 
-                  class="p-1 border border-gray-300 hover:underline hover:cursor-pointer hover:bg-gray-200">{{
-                    item.title }}</td>
+                  class="p-1 border border-gray-300 hover:underline hover:cursor-pointer hover:bg-gray-200">{{ item.idx }}</td>
+                  <td 
+                  class="p-1 border border-gray-300 hover:underline hover:cursor-pointer text-center hover:bg-gray-200">
+                  {{ item.title.length > 20 ? item.title.substring(0, 20) + '...' : item.title }}
+                </td>
                 <td class="p-1 border border-gray-300">{{ item.user }}</td>
-                <td class="p-1 border border-gray-300">{{ item.wdate }}</td>
-                <td class="p-1 border border-gray-300">{{ item.lecture }}</td>
+                <td class="p-1 border border-gray-300">{{ item.wdate  }}</td>
+                <td class="p-1 border border-gray-300">{{  }}</td>
               </tr>
             </tbody>
           </table>
         </div>
 
-        <button  class="px-4 py-2 text-white bg-green-600 rounded hover:opacity-80 mr-3">최신순</button>
-        <button  class="px-4 py-2 text-white bg-blue-600 rounded hover:opacity-80">과거순</button>
+        <button @click="sortAsc" class="px-4 py-2 mr-3 text-white bg-green-600 rounded hover:opacity-80">최신순</button>
+        <button @click="sortDesc" class="px-4 py-2 text-white bg-blue-600 rounded hover:opacity-80">과거순</button>
 
         <div class="flex justify-center mt-5 space-x-2">
-          <button 
+          <button @click="prevPageGroup" :disabled="currentPageGroup === 0"
             class="px-3 py-1 bg-white border border-gray-300 hover:bg-gray-100">&lt;</button>
           <span v-for="page in currentPageNumbers" :key="page"
             class="px-3 py-1 border border-gray-300 cursor-pointer hover:bg-gray-100"
-            >
-            {{  }}
+            @click="(ascdesc) ? getPage(page) : getdescPage(page)">
+            {{ page }}
           </span>
-          <button 
+          <button @click="nextPageGroup" :disabled="currentPageGroup >= maxPageGroup"
             class="px-3 py-1 bg-white border border-gray-300 hover:bg-gray-100">&gt;</button>
         </div>
       </section>
