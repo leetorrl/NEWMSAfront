3e4950738f31c4591601e415eeaa52468074faea
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-12-27 18:12:58 +0900

    no message

--- teamprojectvue/src/api/utils.js
+++ teamprojectvue/src/api/utils.js
@@ -8,5 +8,5 @@
 // export const GLOBAL_URL = import.meta.env.VITE_API_URL || 'http://greencomart.kro.kr:716';
 export const GLOBAL_URL = 'http://192.168.0.103:80';
 export const GLOBAL_ANNOUNCE_URL = GLOBAL_URL;
-export const GLOBAL_QNA_URL = GLOBAL_URL;
-// export const GLOBAL_QNA_URL = import.meta.env.VITE_API_QNA_URL || 'http://192.168.0.69:8120';
+// export const GLOBAL_QNA_URL = GLOBAL_URL;
+export const GLOBAL_QNA_URL = import.meta.env.VITE_API_QNA_URL || 'http://192.168.0.69:8120';
