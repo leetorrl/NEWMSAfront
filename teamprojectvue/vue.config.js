59a00acbe4c05604218d993587a6b3ae68c75e01
suidori <leo7750@naver.com>
2025-02-03 16:46:05 +0900

    0203

--- teamprojectvue/vue.config.js
+++ teamprojectvue/vue.config.js
@@ -1,5 +1,9 @@
 // vue.config.js
 module.exports = {
+  publicPath: './', // 상대 경로로 설정 (배포 시 필요한 경우)
+  devServer: {
+    historyApiFallback: true // 모든 요청을 index.html로 리디렉션
+  },
   pwa: {
     name: 'My PWA App',
     themeColor: '#4DBA87',
